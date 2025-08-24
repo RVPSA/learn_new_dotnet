7f74935b11a27172fd037ae8dbd6a79144198f1c
Sachintha Akesh <akeshramasinghe@gmail.com>
2025-08-24 13:15:40 +0530

    Initial comment

---
+++ DotNetBackgroundService/DotNetBackgroundService/Models/Room.cs
@@ -0,0 +1,24 @@
+﻿namespace DotNetBackgroundService.Models
+{
+    public class Room
+    {
+        public int NumberOfOccupents { get; set; } = 0;
+        public List<string> Furnitures { get; set; } = new List<string>();
+        public bool HasLight { get; set; } = true;
+        public bool IsLightOn { get; set; } = false;
+
+        //Checking Light On/ Off
+        public bool IsDark() {
+            return IsLightOn ? false : true;
+        }
+
+        public List<string> GetAllFurnitures() {
+            return Furnitures;
+        }
+
+        public List<string> AddFurniture(string furnitureName) {
+            Furnitures.Add(furnitureName);
+            return Furnitures;
+        }
+    }
+}
